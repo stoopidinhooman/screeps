54162ebea4d4622168a047db00dd16bf0a655a9e
Amos Christian <amos.christian@hreasily.com>
2016-11-28 23:25:11 +0700

    Add new module role.upgrader

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function() {
 	for(var name in Game.creeps){
 		var creep = Game.creeps[name];
-		roleHarvester.run(creep, 'Base');
+		if(creep.memory.role == 'harvester'){
+			roleHarvester.run(creep, 'Base');
+		}
+		if(creep.memory.role == 'upgrader'){
+			roleUpgrader.run(creep);
+		}
+
 	}
 }
---
+++ role.upgrader.js
@@ -0,0 +1,28 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.harvester');
+ * mod.thing == 'a thing'; // true
+ */
+ 
+ var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester;
